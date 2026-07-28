262e5c0c6dbc0959006fac7aeabdccb72bffd878
David Lucas <davidlucswt002x@gmail.com>
2026-07-28 15:18:42 -0300

    Removendo: Aviso Caso Não Encontre o CEP

--- src/gui_web/script.js
+++ src/gui_web/script.js
@@ -657,7 +657,6 @@ async function buscarCEPApi(cepFormatado, sufixo = '') {
 
         if (dados.erro) {
             inputCep.classList.add('campo-invalido');
-            alert("CEP não encontrado! Por favor, verifique a digitação.");
             document.getElementById('rua' + sufixo).value = "";
             document.getElementById('bairro' + sufixo).value = "";
             document.getElementById('cidade' + sufixo).value = "";
